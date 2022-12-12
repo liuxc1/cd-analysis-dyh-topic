85eb0aa18866c02214b7e7bd7db349637dbb3b76
刘现成 <921119098@qq.com>
2022-12-12 16:05:35 +0800

    56666666666

--- src/main/java/ths/project/analysis/airqualitystatus/mapper/AirQualityStatusMapper.java
+++ src/main/java/ths/project/analysis/airqualitystatus/mapper/AirQualityStatusMapper.java
@@ -6,6 +6,6 @@ import java.util.Map;
  * @author lx
  * @date 2021年09月09日 11:42
  */
-public interface AirQualityStatusMapper {
+public interface AirQualityStatusMapper   {
     Map<String, String> queryMaxDate();
 }
